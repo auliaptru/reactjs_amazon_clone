68a6151b8b49a1c0f9aea42f1bcda851a088d7ac
Aulia Putri Utami <aulia1700016113@webmail.uad.ac.id>
2024-02-05 22:44:06 +0700

    update Sale page (2)

--- src/pages/Navbar/Sell/Sell.jsx
+++ src/pages/Navbar/Sell/Sell.jsx
@@ -79,14 +79,131 @@ const Sell = () => {
                                 <div className='border__img'></div>
                             </div>
                             <div className='review__text-wrapper'>
-                                <h3 className='review__text'>
+                                <h3 className='review__quote'>
                                     "I’ve actually been really impressed—and
                                     kind of surprised, quite frankly—at how much
                                     Amazon has helped us build the brand; not
                                     just on Amazon, but outside of Amazon."
                                 </h3>
+                                <div className='review__founder'>
+                                    <div className='founder__img'>
+                                        <img
+                                            src='https://m.media-amazon.com/images/G/01/sell/images/headshots/mmccauley-2x.png'
+                                            alt=''
+                                        />
+                                    </div>
+                                    <div className='founder__name-wrapper'>
+                                        <span className='founder__name'>
+                                            Matt McCauley
+                                        </span>
+                                        <span className='founder__title'>
+                                            Co-Founder
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='review__discovery-stats'>
+                                <div className='stats__first'>
+                                    <h3>
+                                        <strong>74%</strong>
+                                    </h3>
+                                    <p>
+                                        of Amazon Customers use Amazon to
+                                        discover new products or brands
+                                    </p>
+                                </div>
+                                <div className='stats__second'>
+                                    <h3>
+                                        <strong>500K</strong>
+                                    </h3>
+                                    <p>
+                                        brands are in Brand Registry, unlocking
+                                        our powerful tools
+                                    </p>
+                                </div>
                             </div>
                         </div>
+                    </div>
+                </div>
+                <div className='sell__incentives'>
+                    <div className='sell__incentives-left'>
+                        <button>
+                            <div className='icon'>
+                                <img
+                                    src='https://m.media-amazon.com/images/G/01/sell/images/icon-magic-sparkle-white.svg'
+                                    alt=''
+                                />
+                            </div>
+                            <span>New Seller Incentives</span>
+                            <div className='icon'>
+                                <img
+                                    src='https://m.media-amazon.com/images/G/01/sell/images/icon-magic-sparkle-white.svg'
+                                    alt=''
+                                />
+                            </div>
+                        </button>
+                        <h1>Over $50K in potential benefits</h1>
+                        <p>
+                            Ready to sell? Launch your brand today with a
+                            powerful playbook for new sellers and over $50K in
+                            potential benefits.
+                        </p>
+                        <div className='incentives__benefits'>
+                            <div className='incentives__benefits-wrapper'>
+                                <div className='checkIcon'>
+                                    <img
+                                        src='https://m.media-amazon.com/images/G/01/sell/images/icon-check-blue-thick.svg'
+                                        alt=''
+                                    />
+                                </div>
+                                <span>
+                                    Get 5% back on your first $1,000,000 in
+                                    branded sales
+                                </span>
+                            </div>
+                            <div className='incentives__benefits-wrapper'>
+                                <div className='checkIcon'>
+                                    <img
+                                        src='https://m.media-amazon.com/images/G/01/sell/images/icon-check-blue-thick.svg'
+                                        alt=''
+                                    />
+                                </div>
+                                <span>
+                                    Try FBA with free inbound shipping, storage,
+                                    removals, and returns
+                                </span>
+                            </div>
+                            <div className='incentives__benefits-wrapper'>
+                                <div className='checkIcon'>
+                                    <img
+                                        src='https://m.media-amazon.com/images/G/01/sell/images/icon-check-blue-thick.svg'
+                                        alt=''
+                                    />
+                                </div>
+                                <span>
+                                    Get $50 credit for Sponsored Products CPC
+                                    ads—bids are up to you
+                                </span>
+                            </div>
+                            <div className='incentives__benefits-wrapper'>
+                                <div className='checkIcon'>
+                                    <img
+                                        src='https://m.media-amazon.com/images/G/01/sell/images/icon-check-blue-thick.svg'
+                                        alt=''
+                                    />
+                                </div>
+                                <span>
+                                    Get a head start on brand protection,
+                                    <a href=''>product reviews,</a>and more
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='sell__incentives-right'>
+                        <img
+                            src='https://m.media-amazon.com/images/G/01/sell/images/illustration/illustration-growth-graph.svgg'
+                            alt=''
+                        />
                     </div>
                 </div>
             </div>
